14d899002742334e0c1cf666d89b0a12a92264d0
martin <martin@V-DOXIE-W10>
2016-07-11 11:16:23 +0800

    Add project files.

---
+++ BackgroundTaskDemo/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BackgroundTaskDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var exampleTaskName = "BackgroundTask1";
+            foreach (var mytask in BackgroundTaskRegistration.AllTasks)
+            {
+                if (mytask.Value.Name == exampleTaskName)
+                {
+                    return;
+                }
+            }
+            var builder = new BackgroundTaskBuilder();
+            builder.Name = exampleTaskName;
+            builder.TaskEntryPoint = "RuntimeTask.Task1";
+
+            IBackgroundTrigger triggter = new SystemTrigger(SystemTriggerType.TimeZoneChange, false);
+            builder.SetTrigger(triggter);
+
+            BackgroundTaskRegistration task = builder.Register();
+            task.Completed += new BackgroundTaskCompletedEventHandler(OnTask1Completed);
+        }
+
+        private void CancelTask1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+                if (String.Equals(cur.Value.Name, "BackgroundTask1")) {
+                    cur.Value.Unregister(true);
+                }  
+            }
+        }
+
+
+        private void OnTask1Completed(IBackgroundTaskRegistration task, BackgroundTaskCompletedEventArgs args)
+        {
+            var settings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            var message = settings.Values["Task1"].ToString();
+            Debug.WriteLine(message);
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var exampleTaskName = "BackgroundTask2";
+            foreach (var mytask in BackgroundTaskRegistration.AllTasks)
+            {
+                if (mytask.Value.Name == exampleTaskName)
+                {
+                    return;
+                }
+            }
+            var builder = new BackgroundTaskBuilder();
+            builder.Name = exampleTaskName;
+            builder.TaskEntryPoint = "RuntimeTask.Task2";
+
+
+            IBackgroundTrigger triggter = new SystemTrigger(SystemTriggerType.TimeZoneChange, false);
+            builder.SetTrigger(triggter);
+
+            BackgroundTaskRegistration task = builder.Register();
+            task.Completed += new BackgroundTaskCompletedEventHandler(OnTask2Completed);
+        }
+
+
+
+        private void CancelTask2_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+                if (String.Equals(cur.Value.Name, "BackgroundTask2"))
+                {
+                    cur.Value.Unregister(true);
+                }
+            }
+        }
+
+        private void OnTask2Completed(IBackgroundTaskRegistration task, BackgroundTaskCompletedEventArgs args)
+        {
+            var settings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            var message = settings.Values["Task2"].ToString();
+            Debug.WriteLine(message);
+
+        }
+    }
+}
---
+++ RuntimeTask/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+using Windows.System.Threading;
+
+namespace RuntimeTask
+{
+    public sealed partial class Task1 : IBackgroundTask
+    {
+        BackgroundTaskDeferral _deferral = null;
+        IBackgroundTaskInstance _taskInstance = null;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+            _taskInstance = taskInstance;
+            Debug.WriteLine("Background " + taskInstance.Task.Name + " Starting...");
+
+            var settings = ApplicationData.Current.LocalSettings;
+            settings.Values["Task1"] = "this is Task1 data";
+            _deferral.Complete();
+        }
+    }
+    public sealed partial class Task2 : IBackgroundTask
+    {
+        BackgroundTaskDeferral _deferral = null;
+        IBackgroundTaskInstance _taskInstance = null;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+            _taskInstance = taskInstance;
+            Debug.WriteLine("Background " + taskInstance.Task.Name + " Starting...");
+
+            var settings = ApplicationData.Current.LocalSettings;
+            settings.Values["Task2"] = "this is Task2 data";
+            _deferral.Complete();
+        }
+    }
+}
